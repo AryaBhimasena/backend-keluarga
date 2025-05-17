f76a8e0239419fbad71241e9096bf881a1e6558d
Iyaz <ranggaliro@gmail.com>
2025-05-17 20:27:41 +0800

    Backend Keluarga - 6

--- db.js
+++ db.js
@@ -1,28 +1,25 @@
-import mysql from 'mysql2/promise';
+// db.js
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0, // biar antrian tidak dibatasi, bisa kamu atur nanti
-  connectTimeout: 10000, // timeout koneksi 10 detik
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
-// Misalnya di file db.js
+// Tes koneksi
 connection.connect((err) => {
   if (err) {
     console.error('❌ Gagal konek DB:', err.message);
-    process.exit(1); // jangan biarkan lanjut jika DB gagal
+    process.exit(1); // Keluar dari aplikasi jika gagal konek
   } else {
     console.log('✅ Koneksi ke database berhasil');
   }
 });
 
-export default pool;
+export default connection;
