65c25ae18f08856def3e197bb4efd4cc4156ecc6
Iyaz <ranggaliro@gmail.com>
2025-05-17 20:45:34 +0800

    Backend Keluarga - 7

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 // db.js
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const db = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
@@ -12,17 +12,5 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT,
 });
 
-// Tes koneksi
-connection.connect((err) => {
-  if (err) {
-    console.error('❌ Gagal konek DB:', err.message);
-    process.exit(1); // Keluar dari aplikasi jika gagal konek
-  } else {
-    console.log('✅ Koneksi ke database berhasil');
-  }
-});
-
-// Ubah ke promise wrapper
-const db = connection.promise();
-
-export default connection;
+console.log('✅ Koneksi ke database berhasil');
+export default db;
